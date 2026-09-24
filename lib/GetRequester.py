667a4c75ad8212a4c20959e7431eb3d99bb54d98
Gumdrop <david.nyongesa@student.moringaschool.com>
2026-09-24 21:52:09 +0300

    Implement GET request and JSON loading methods

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        """Send a GET request to self.url and return the raw response body (bytes)."""
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        """Parse the response body into Python data (a list of dicts)."""
+        return json.loads(self.get_response_body())
